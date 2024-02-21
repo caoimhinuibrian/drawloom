e6b4a52b57bdf7bfade0f984b1fb3dc955288647
Kevin O'Brien <12157865+caoimhinuibrian@users.noreply.github.com>
2024-02-20 14:48:39 -1000

    Initial Commit

---
+++ drawloom/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  drawloom
+//
+//  Created by Kevin O'Brien on 2/20/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ drawloom/drawloomApp.swift
@@ -0,0 +1,17 @@
+//
+//  drawloomApp.swift
+//  drawloom
+//
+//  Created by Kevin O'Brien on 2/20/24.
+//
+
+import SwiftUI
+
+@main
+struct drawloomApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
